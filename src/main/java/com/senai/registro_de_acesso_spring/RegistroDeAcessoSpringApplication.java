09906e89f9a33f4a74e0bf8e8f6dfb18a47629d9
daniel <dmoncao7@gmail.com>
2025-04-03 14:08:33 -0300

    Initial commit

---
+++ src/main/java/com/senai/registro_de_acesso_spring/RegistroDeAcessoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.senai.registro_de_acesso_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroDeAcessoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroDeAcessoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/registro_de_acesso_spring/RegistroDeAcessoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.registro_de_acesso_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroDeAcessoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
