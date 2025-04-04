40b5fbeac23bcf4e14f3f98009bdad1f639e1b3b
daniel <dmoncao7@gmail.com>
2025-04-04 17:08:50 -0300

    repositorio funcionando

--- src/main/java/com/senai/registro_de_acesso_spring/repository/UsuarioRepository.java
+++ src/main/java/com/senai/registro_de_acesso_spring/repository/UsuarioRepository.java
@@ -1,8 +1,9 @@
 package com.senai.registro_de_acesso_spring.repository;
 
+import com.senai.registro_de_acesso_spring.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository  {
+public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
 }
