92fc69dcb421aecca5c185a499be7b06013ad65f
daniel <dmoncao7@gmail.com>
2025-04-03 22:10:57 -0300

    definição de entidades em andamento

--- src/main/java/com/senai/registro_de_acesso_spring/Controller/UsuarioController.java
+++ src/main/java/com/senai/registro_de_acesso_spring/Controller/UsuarioController.java
@@ -1,11 +1,14 @@
 package com.senai.registro_de_acesso_spring.Controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/usuarios")
 public class UsuarioController {
+
+
 
 }
