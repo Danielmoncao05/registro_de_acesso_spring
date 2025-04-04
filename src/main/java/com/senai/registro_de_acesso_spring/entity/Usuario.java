0b78d58fd1d51dac4a1ff69802acd218cc96b086
daniel <dmoncao7@gmail.com>
2025-04-03 22:11:55 -0300

    inserindo atributos ao usuario, falta determinar se torna abstrato ou nao

--- src/main/java/com/senai/registro_de_acesso_spring/entity/Usuario.java
+++ src/main/java/com/senai/registro_de_acesso_spring/entity/Usuario.java
@@ -1,4 +1,85 @@
 package com.senai.registro_de_acesso_spring.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private Integer idAcesso;
+    private String CPF;
+    private String nome;
+    private String email;
+    private String senha;
+    private String telefone;
+
+    public Usuario(int id, Integer idAcesso, String CPF, String nome, String email, String senha, String telefone) {
+        this.id = id;
+        this.idAcesso = idAcesso;
+        this.CPF = CPF;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.telefone = telefone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Integer getIdAcesso() {
+        return idAcesso;
+    }
+
+    public void setIdAcesso(Integer idAcesso) {
+        this.idAcesso = idAcesso;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
