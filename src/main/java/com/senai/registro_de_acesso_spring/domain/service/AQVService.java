9ab1da667a0dee812848312fd0c7d896dd2d49e7
Giovanna Queiróz <giovannasantos0616@gmail.com>
2025-05-09 17:12:53 -0300

    Inicio da configuração do UsuarioService e UsuarioController - 09/05/2025

--- src/main/java/com/senai/registro_de_acesso_spring/domain/service/AQVService.java
+++
@@ -1,7 +0,0 @@
-package com.senai.registro_de_acesso_spring.domain.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AQVService {
-}
--- src/main/java/com/senai/registro_de_acesso_spring/interface_ui/controller/UsuarioController.java
+++ src/main/java/com/senai/registro_de_acesso_spring/interface_ui/controller/UsuarioController.java
@@ -2,9 +2,14 @@ package com.senai.registro_de_acesso_spring.interface_ui.controller;
 
 import com.senai.registro_de_acesso_spring.application.services.UsuarioService;
 import com.senai.registro_de_acesso_spring.application.dto.UsuarioDTO;
+import com.senai.registro_de_acesso_spring.domain.entity.usuarios.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuario")
@@ -12,55 +17,57 @@ public class UsuarioController {
         // mover por service
     @Autowired
     UsuarioService usuarioService;
-/*
-    @GetMapping
-    public List<Usuario> getUsers() {
-        return userRepository.findAll();
-    }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getUserById(@PathVariable Long id) {
-        Optional<Usuario> user = userRepository.findById(id);
-        return user.map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
-    }
-*/
     @PostMapping
     public ResponseEntity<Void> cadastrarUsuario(@RequestBody UsuarioDTO dto) {
         usuarioService.cadastrarUsuario(dto);
         return ResponseEntity.ok().build();
     }
-/*
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>>listarUsuarios(@RequestBody UsuarioDTO dto) {
+        return ResponseEntity.ok(usuarioService.listarUsuarios(dto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> listarUsuarioPorId(@PathVariable Long id) {
+        Optional<Usuario> user = usuarioService.findById(id);
+        return user.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     @PutMapping("/{id}")
-    public ResponseEntity<Usuario> updateUser(@PathVariable Long id, @RequestBody Usuario userUpdated) {
-        Optional<Usuario> userExistents = userRepository.findById(id);
+    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuarioAtualizado) {
+        Optional<Usuario> usuarioExistente = usuarioService.findById(id);
 
-        if(userExistents.isPresent()) {
-            Usuario user = userExistents.get();
+        if(usuarioExistente.isPresent()) {
+            Usuario usuario = usuarioExistente.get();
 
-            user.setIdDeAcesso(userUpdated.getIdDeAcesso());
-            user.setNome(userUpdated.getNome());
-            user.setTelefone(userUpdated.getTelefone());
-            user.setEmail(userUpdated.getEmail());
-            user.s
+            usuario.setIdAcesso(usuarioAtualizado.getIdAcesso());
+            usuario.setNome(usuarioAtualizado.getNome());
+            usuario.setCpf(usuarioAtualizado.getCpf());
+            usuario.setDataNascimento(usuarioAtualizado.getDataNascimento());
+            usuario.setTelefone(usuarioAtualizado.getTelefone());
+            usuario.setEmail(usuarioAtualizado.getEmail());
+            usuario.setSenha(usuarioAtualizado.getSenha());
 
-            userRepository.save(user);
+            usuarioService.save(usuario);
 
-            return ResponseEntity.ok(user);
+            return ResponseEntity.ok(usuario);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-        if(userRepository.existsById(id)) {
-            userRepository.deleteById(id);
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
+        if(usuarioService.existsById(id)) {
+            usuarioService.deleteById(id);
 
             return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
             return ResponseEntity.notFound().build();
         }
-    }*/
-
+    }
+*/
 }
